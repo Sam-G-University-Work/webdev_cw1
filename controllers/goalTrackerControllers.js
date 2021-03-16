b2e1da650972f2d0496b17fc7d149cbdb91704bb
“onepoint16” <everythingsaremix@gmail.com>
2021-03-16 16:53:14 +0000

    Embedded DB

--- controllers/goalTrackerControllers.js
+++ controllers/goalTrackerControllers.js
@@ -1,7 +1,7 @@
 const { response } = require('express');
 const goalTrackerDAO = require('../models/goalTrackerModel');
 const db = new goalTrackerDAO();
-//const db = new GoalsDAO('newgoals.db');
+//const db = new goalTrackerDAO('newgoals.db');
 
 exports.landing_page = function(req, res) {
     db.init();
